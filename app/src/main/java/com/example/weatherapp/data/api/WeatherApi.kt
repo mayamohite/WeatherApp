22baba6396fbc063ad7197682638ae4915e3f9d5
mayamohite <mayapmohite@gmail.com>
2022-01-20 18:14:01 +0530

    Update api

--- app/src/main/java/com/example/weatherapp/data/api/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/data/api/WeatherApi.kt
@@ -1,26 +1,27 @@
 package com.example.weatherapp.data.api
 
-import com.example.weatherapp.data.entities.WeatherResponse
+import com.example.weatherapp.data.api.response.CurrentWeatherResponse
+import com.example.weatherapp.data.api.response.ForecastResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
  * Retrofit api calls
  */
-private const val KEY = "55SC5PB4HZNETSR8MDYB9MLAY"
-private const val CONTENT_TYPE = "json"
 
 interface WeatherApi {
 
-    @GET("/{city_name}/{time_period}?")
-    suspend fun getWeatherForecast(
-        @Path("city_name") cityName: String,
-        @Path("time_period") time: String,
-        @Query("unitGroup") unitGroup: String,
-        @Query("elements") elements: String,
-        @Query("include") include: String,
-        @Query("key") key: String = KEY,
-        @Query("contentType") contentType: String = CONTENT_TYPE,
-        ): WeatherResponse
+    @GET("forecast")
+   suspend fun getForecastByGeoCords(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("units") units: String
+    ): ForecastResponse
+
+    @GET("weather")
+    suspend fun getCurrentByGeoCords(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("units") units: String
+    ): CurrentWeatherResponse
 }
